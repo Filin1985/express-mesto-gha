aa681bb7373bb29ae3dec55113dc242f4836b997
Marat Ikhsanov <ihsmen@gmail.com>
2023-07-03 19:29:22 +0300

    Добавить ссылку на репозиторий.

--- controllers/cards.js
+++ controllers/cards.js
@@ -38,9 +38,7 @@ module.exports.createNewCard = async (req, res) => {
   try {
     const owner = req.user._id;
     const { name, link } = req.body;
-    console.log(name, link, owner);
     const newCard = await Card.create({ name, link, owner });
-    console.log(newCard);
     res.send({ newCard });
   } catch (err) {
     if (err.name === "ValidationError") {
@@ -61,12 +59,18 @@ module.exports.deleteCard = async (req, res) => {
   try {
     const { cardId } = req.params;
     const card = await Card.findByIdAndRemove({ _id: cardId });
+    if (!card) {
+      throw new NotFoundError("Такого id не существует!", "NotFoundError");
+    }
     res.send({ card });
   } catch (err) {
     if (err.name === "CastError") {
       return res
-        .status(NOT_FOUND_ERROR)
+        .status(INCORRECT_DATA_ERROR)
         .send({ message: "Запрашиваемая карточка не найдена!" });
+    }
+    if (err.errorName === "NotFoundError") {
+      return res.status(err.status).send({ message: err.message });
     }
     console.log(
       `Статус ${err.status}. Ошибка ${err.name} c текстом ${err.message}`
