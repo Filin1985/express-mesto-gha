78c36816ade588ad866b84e144d7f2137b700dbf
Marat Ikhsanov <ihsmen@gmail.com>
2023-07-16 18:06:48 +0300

    Добавить функционал авторизации и логина.

--- controllers/cards.js
+++ controllers/cards.js
@@ -27,9 +27,7 @@ module.exports.deleteCard = async (req, res, next) => {
   try {
     const { cardId } = req.params;
     const userId = req.user._id;
-    console.log(userId);
     const card = await Card.findByIdAndRemove({ _id: cardId });
-    console.log(card.owner._id);
     if (!card) {
       throw new NotFoundError('Такого id не существует!', 'NotFoundError');
     }
