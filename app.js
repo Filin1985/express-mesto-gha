d4c906d6430e3ecd83ecb6fd28da9eaef560eee0
Marat Ikhsanov <ihsmen@gmail.com>
2023-07-16 16:00:30 +0300

    Добавить функционал авторизации и логина.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable import/no-extraneous-dependencies */
 require('dotenv').config();
 const { celebrate, Joi } = require('celebrate');
@@ -12,6 +13,8 @@ const errorHandler = require('./middlewares/errorHandler');
 const { login, createNewUser } = require('./controllers/users');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+
+const REG_EXP = '/(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?\/[a-zA-Z0-9]{2,}|((https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z]{2,}(\.[a-zA-Z]{2,})(\.[a-zA-Z]{2,})?)|(https:\/\/www\.|http:\/\/www\.|https:\/\/|http:\/\/)?[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}\.[a-zA-Z0-9]{2,}(\.[a-zA-Z0-9]{2,})?/g';
 
 mongoose
   .connect(DB_URL, {
@@ -33,7 +36,7 @@ app.post('/signup', celebrate({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(8),
     name: Joi.string().min(2).max(30),
-    avatar: Joi.string().min(2).max(30).url(),
+    avatar: Joi.string().pattern(new RegExp(REG_EXP)).min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), createNewUser);
